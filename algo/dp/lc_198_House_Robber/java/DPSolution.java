4a7e01ef4b0b64813b990e9355c3d34f694d16a9
coder in mountain <coder.in.mountain@gmail.com>
2021-11-17 15:50:49 -0800

    adding java solution

---
+++ algo/dp/lc_198_House_Robber/java/DPSolution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+
+        int[] profit = new int[n];
+        profit[0] = nums[0];
+        profit[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            profit[i] = Math.max(profit[i-2] + nums[i], profit[i-1]);
+        }
+        return profit[n-1];
+    }
+}
