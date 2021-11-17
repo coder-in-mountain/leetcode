b43514aa59d3cc789271732ecb2a8ace21be326d
coder in mountain <coder.in.mountain@gmail.com>
2021-11-17 12:25:13 -0800

    adding java solution

---
+++ algo/dp/lc_746_Min_Cost_Climbing_Stairs/java/DPSolution.java
@@ -0,0 +1,23 @@
+/*
+You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+You can either start from the step with index 0, or the step with index 1.
+Return the minimum cost to reach the top of the floor.
+*/
+class Solution {
+
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        /*
+         dp[i] is the minimium cost to get to step i.
+         After paying cost[i], we can take one or two steps more.
+         */
+        int[] dp = new int[n+1];
+
+        for (int i = 2; i <= n; i++) {
+            int takeOneStep = dp[i-1] + cost[i-1];
+            int takeTwoSteps = dp[i-2] + cost[i-1];
+            dp[i] = Math.min(takeOneStep, takeTwoSteps);
+        }
+        return dp[n];
+    }
+}
